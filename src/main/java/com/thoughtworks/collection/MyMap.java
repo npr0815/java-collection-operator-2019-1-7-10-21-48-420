e7c1fc7a8f9396336a0707e0bce4470f562e10d9
nn <npr_0815@163.com>
2019-05-17 01:49:08 +0800

    finished MyMap.java

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MyMap {
@@ -17,22 +19,62 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i : array){
+            list.add(i*3);
+        }
+
+        return list;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> list = new ArrayList<>();
+
+        for (int i : array){
+            int num = i + 96;
+            char c = (char)num;
+            list.add(Character.toString(c));
+        }
+
+        return list;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> list = new ArrayList<>();
+
+        for (int i : array){
+            if (i < 27){
+                int num = i + 96;
+                char c = (char)num;
+                list.add(Character.toString(c));
+            } else if (i > 27 && i % 26 == 0){
+                int n = i / 26 - 1 + 96;
+                int num = i % 26 + 26 + 96;
+                char c = (char)n;
+                char ch = (char)num;
+                list.add(""+c+ch);
+            } else {
+                int n = i / 26 + 96;
+                int num = i % 26 + 96;
+                char c = (char)n;
+                char ch = (char)num;
+                list.add(""+c+ch);
+            }
+        }
+
+        return list;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        Collections.reverse(array);
+
+        return array;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        Collections.sort(array);
+
+        return array;
     }
 }
