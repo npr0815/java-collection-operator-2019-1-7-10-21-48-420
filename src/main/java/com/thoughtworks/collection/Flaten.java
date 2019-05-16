f53d8359e459ae86c3b8d5616143265d24342a64
nn <npr_0815@163.com>
2019-05-17 01:46:23 +0800

    finished Flaten.java

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
+import java.util.*;
 
 public class Flaten {
 
@@ -12,10 +12,43 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        int len = 0, index = 0;
+        for (Integer[] i : array){
+            len += i.length;
+        }
+        Integer[] arr = new Integer[len];
+
+        for (Integer[] i : array) {
+            for (int j : i) {
+                arr[index++] = j;
+            }
+        }
+
+        List<Integer> list = Arrays.asList(arr);
+
+        return list;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        int len = 0, index = 0;
+        List<Integer> list = new ArrayList<>();
+        for (Integer[] i : array){
+            len += i.length;
+        }
+        Integer[] arr = new Integer[len];
+
+        for (Integer[] i : array) {
+            for (int j : i) {
+                arr[index++] = j;
+            }
+        }
+
+        for (int i : arr){
+            if (!list.contains(i)){
+                list.add(i);
+            }
+        }
+
+        return list;
     }
 }
